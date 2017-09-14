9f37168fcd878de58745f16710a8ef28da2b3c18
Nicholas Lamicela <nupanick@gmail.com>
2017-09-14 23:02:05 +0000

    Initial commit.

---
+++ ex1.scm
@@ -0,0 +1,22 @@
+;;; SICP exercise 1.3
+;;; Define a procedure that takes three numbers as arguments and
+;;; returns the sum of the squares of the two larger numbers.
+(define (square-top-two a b c)
+  ;; helper function, sum two.
+  (define (sum-of-squares x y)
+    (define (square z)
+      (* z z))
+    (+ (square x) (square y)))
+  ;; determine which number is smallest, and sum the other two.
+  (if (< a b)
+    (if (< a c)
+      ;; a is the smallest
+      (sum-of-squares b c)
+      ;; c is the smallest
+      (sum-of-squares a b))
+    (if (< b c)
+      ;; b is the smallest
+      (sum-of-squares a c)
+      ;; c is the smallest
+      (sum-of-squares a b))))
+
